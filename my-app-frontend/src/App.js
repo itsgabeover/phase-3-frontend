83bc7f089730a07fd1d587a540979825555beaa4
Gabriel Rodriguez <GabeCreaCS@gmail.com>
2021-10-06 09:58:06 -0700

    Routes added

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
 import UsersContainer from './components/UsersContainer';
+import React, {useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom"
+import  NavBar  from "./Components/NavBar"
+import  HomePage  from "./Components/HomePage"
 
 function App() {
+
+
+ const [ users, setUsers] = useState([])
+
+    const getUsers = () => {
+        fetch("http://localhost:9292/users")
+        .then(resp => resp.json())
+        .then(usersData => setUsers(usersData))
+      }
+
+
+    const renderNewUsers = (newUser) => {
+      console.log(usersData)
+      setUsers([...users, newUser])
+     
+    }
+
+
+
   return (
     <div className="App">
-      <UsersContainer />
+    <NavBar onAddEvent={handleAddEvent}/>
+      <Switch>
+
+        <Route path="/">
+             <Login />
+        </Route >
+
+        <Route path="/HomePage">
+            <HomePage />
+        </Route>
+
+        <Route path="/Friends">
+            <Friends />
+        </Route>
+
+        <Route path="/Games">
+            <Games />
+        </Route >
+    
+      </Switch>
     </div>
   );
 }
--- my-app-frontend/src/components/UsersContainer.js
+++ my-app-frontend/src/components/UsersContainer.js
@@ -1,28 +1,4 @@
 import React from 'react'
 
-function UsersContainer() {
-
-    const getUsers = () => {
-        fetch("http://localhost:9292/users")
-        .then(resp => resp.json())
-        .then(usersArray=> console.log(usersArray))
-      }
-
-    const renderUsers = (usersArray) => {
-        return (
-            <div>hi</div>
-        )
-    }
-
-    debugger;
-
-    return (
-      <div>
-        LOOK AT ALL THESE USERS
-        
-      </div>
-    );
-  }
-  
   export default UsersContainer;
   
