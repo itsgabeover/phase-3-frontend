2e5b436ece34db75f261b82e2bc8d6a828991cee
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-06 11:48:54 -0700

    App.js updates for server routes

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import UsersContainer from './components/UsersContainer';
-import React, {useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+// import UsersContainer from './components/UsersContainer';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import  NavBar  from "./components/NavBar"
 import  HomePage  from "./components/HomePage"
 import Login from './components/Login';
@@ -11,22 +11,30 @@ import Games from './components/Games';
 function App() {
 
 
- const [ users, setUsers] = useState([])
+//  const [ users, setUsers] = useState([])
 
     const getUsers = () => {
         fetch("http://localhost:9292/users")
         .then(resp => resp.json())
-        .then(usersData => setUsers(usersData))
+        .then(usersData => console.log(usersData))
+        // .then(usersData => setUsers(usersData))
       }
 
-
+    
     // const renderNewUsers = (newUser) => {
     //   console.log(usersData)
     //   setUsers([...users, newUser])
      
     // }
 
+    const getUser = (id) => {
+      fetch(`http://localhost:9292/users/${id}`)
+      .then(resp => resp.json())
+      .then(userData => console.log(userData))
+      // .then(userData => setUsers(userData))
+    }
 
+    debugger;
 
   return (
     <div className="App">
