82261a2a77eb23765c3eb7d1f294b080fa20dd4a
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-10-06 11:46:18 -0700

    built login/create new user forms, submit not done

--- my-app-frontend/src/components/Login.js
+++ my-app-frontend/src/components/Login.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Login() {
     return (
-        <div>login</div>
+        <div class="row">
+        <div class="column1">
+            <form className="login-form">
+                <label>Gamertag/E-mail: </label>
+                <input type="text" name="name" className="login-form-input" placeholder="Gamertag/E-mail..." />
+                <label>Password: </label>
+                <input type="text" name="description" className="login-form-input" placeholder="Password..." />
+                <button className="login-button">Login</button>
+            </form>
+        </div>
+        <div class="column2">
+            <form className="login-form">
+                <label>E-mail: </label>
+                <input type="text" name="name" className="login-form-input" placeholder="Enter your e-mail address..." />
+                <label>Gamertag: </label>
+                <input type="text" name="name" className="login-form-input" placeholder="Enter new gamertag..." />
+                <label>Password: </label>
+                <input type="text" name="description" className="login-form-input" placeholder="Enter new password..." />
+                <label>Confirm Password: </label>
+                <input type="text" name="description" className="login-form-input" placeholder="Confirm new password..." />
+                <button className="create-user-button">Create New User</button>
+            </form>
+        </div>
+      </div>
     )
 
 }
