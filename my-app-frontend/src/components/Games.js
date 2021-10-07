b6e8984350bd3a044523cf48c45b7b8a33234dae
Gabriel Rodriguez <GabeCreaCS@gmail.com>
2021-10-07 09:38:20 -0700

    Added fetch for Games

--- my-app-frontend/src/components/Games.js
+++ my-app-frontend/src/components/Games.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Games() {
+
+  const [ games, setGames ] = useState([])
+
+  const getGames = () => {
+    fetch("http://localhost:9292/games")
+    .then(resp => resp.json())
+    .then(gamesData => setGames(gamesData))
+
+  }
+
+  console.log(getGames)
+  
     return (
+
         <div>games</div>
     )
 }
