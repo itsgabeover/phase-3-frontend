a356c03110645d6fcc6acc6a44737fe4be4aa96c
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-07 13:59:45 -0700

    render user info to user component

--- my-app-frontend/src/components/HomePage.js
+++ my-app-frontend/src/components/HomePage.js
@@ -9,35 +9,11 @@ function HomePage({ users }) {
     })
 
     return (
-<<<<<<< HEAD
-        <h1>Hi {userData}</h1>
-        // <div id="homepage">{users.map(user => {
-        //     <div className="profile">
-        //         <h2>{user.name}</h2>
-        //         <img
-        //             src={user.image}
-        //             alt={user.name}
-        //             className="user-avatar"
-        //         />
-        //     </div>
-        // })}</div>
-      );
-
-    // return (
-    //     <div id="homepage">
-    //         <h1>Welcome, {userData.gamer_tag}</h1>
-    //         {/* <h2></h2>
-    //         <img src="{user.image_url}" alt="{user.name}" /> */}
-=======
         <>
         <div>HomePage!!!</div>
         <div>HomePage!!!</div>
         </>
     )
->>>>>>> dev
-
-    //     </div>
-    // )
 }
 
 export default HomePage;
