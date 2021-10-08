2bf2e70d290d09c7d3fe1a2d113d076ee7e211db
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-08 09:20:35 -0700

    add logo

--- my-app-frontend/src/components/NavBar.js
+++ my-app-frontend/src/components/NavBar.js
@@ -7,7 +7,9 @@ function NavBar() {
     return (
         <div className="navbar">
           <div>
-            <h1 onClick={() => history.push("/")}>GAMER GANG</h1>
+            <h1 onClick={() => history.push("/")}>
+              <img id="logo" className="navbarlogo" src="gamer-gang-logo-blue.png" alt="Gamer Gang logo" />
+            </h1>
           </div>
             <h3 className="navbarh3" onClick={() => history.push("/games")}>My Games</h3>
             <h3 className="navbarh3" onClick={() => history.push("/friends")}>My Friends</h3>
